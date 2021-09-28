aa7859f220b97aadfe954d9b76299ad09edb1757
George (Mac) Pence <71496139+georgepence@users.noreply.github.com>
2021-09-28 06:14:14 -0400

    Added React App

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+// const queryAsync = require('./database/dbcon')
+// const fs = require('fs');
+// const { query } = require('express');
+// const apiRoutes = require('./routes/api/api');
+// const dynamicRoutes = require('./routes/api/dynamic');
+// const testRoutes = require('./routes/testing');
+// const coffeeRoutes = require('./routes/coffee');
+// const bookingsRoutes = require('./routes/api/bookings');
+// const employeesRoutes = require('./routes/api/employees');
+// const reservationsRoutes = require('./routes/api/reservations');
+// const petRoutes = require('./routes/api/pets');
+// const ownerRoutes = require('./routes/api/owners');
+// const roomsRoutes = require('./routes/api/rooms');
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+// body parsing middleware
+app.use(express.json());
+
+// use routes
+// app.use('/api', apiRoutes);
+// app.use('/api', testRoutes);
+// app.use('/api/dynamic', dynamicRoutes);
+// app.use('/coffee', coffeeRoutes);
+// app.use('/api/bookings', bookingsRoutes);
+// app.use('/api/employees', employeesRoutes);
+// app.use('/api/reservations', reservationsRoutes);
+// app.use('/api/rooms', roomsRoutes);
+// app.use('/api/pets', petRoutes);
+// app.use('/api/owners', ownerRoutes);
+
+// server static assets if in production
+console.log("running in", process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('mac-app/build'));
+  
+  app.get('*', (req, res) => {
+    // res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'mac-app', 'build', 'index.html'));
+  });
+}
+
+app.get('*', (req, res) => {
+  res.status(500).json({
+    msg: 'Oops this page was not supposed to happen.'
+  })
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on port ${port}.`));
